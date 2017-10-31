1753866d9ecc8a13287155727a572a66aed81e7e
unknown <pierceg83@gmail.com>
2017-10-31 10:28:59 -0700

    Initial commit; using js-dev-env

---
+++ src/vendor.js
@@ -0,0 +1,6 @@
+/* Produces a separate bundle for just vendor libraries, reducing http calls.
+   This remains cached until a vendor updates */
+
+/* eslint-disable no-unused-vars */
+
+import fetch from 'whatwg-fetch';
